6014ebe76e65908bc54ba4cacc9156b2ba5ab665
Wixom78666 <jswix4@gmail.com>
2023-01-04 22:53:45 -0600

    added my mockup readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -47,11 +47,43 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Table of Contents
+  * [License](#license)
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Description
+  ${data.describe}
+
+  ## Installation
+  ${data.install}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## Contributing
+  ${data.contGuide}
+  
+  ## Tests
+  ${data.test}
+  
+  ## Questions
+  If you have quiestions you can reach me at ${data.email}.
+  
+  ## GitHub
+  https://github/${data.github}
 
 `;
 }
